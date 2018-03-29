043935116ecde84971b3f37f7e86342287cab74d
nana80802 <miss_nisreen2000@yahoo.com>
2018-03-29 18:51:40 +0200

    create the spring structure

---
+++ mp8-The-encoder/src/main/java/at/refugeescode/mp8Theencoder/Mp8TheEncoderApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp8Theencoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp8TheEncoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp8TheEncoderApplication.class, args);
+	}
+}
---
+++ mp8-The-morse/src/main/java/at/refugeescode/mp8Themorse/Mp8TheMorseApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp8Themorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp8TheMorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp8TheMorseApplication.class, args);
+	}
+}
